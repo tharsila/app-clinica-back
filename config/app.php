827837638bd5941515a73ad7cd921ed6551820b9
tharsila <tharsilapaula@gmail.com>
2025-03-30 19:39:05 -0300

    feat: adiciona providers JWT Auth e CORS

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
+
     ],
 
     /*
