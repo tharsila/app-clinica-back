3f5c5996090b7be377c75cda5c0f6cf0b647a250
tharsila <tharsilapaula@gmail.com>
2025-03-30 20:04:05 -0300

    feat: adiciona tabeça clinics

---
+++ database/migrations/2025_03_27_211214_create_clinics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClinicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clinics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('regional_id');
+            $table->foreign('regional_id')->references('id')->on('regionals');
+            $table->string('corporate_name');
+            $table->string('trade_name');
+            $table->string('cnpj');
+            $table->date('opening_date');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clinics');
+    }
+}
